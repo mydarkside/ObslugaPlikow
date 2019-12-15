e8c15615fbf7a68b79e691bf9e7c678f5abbc1cc
mydarkside <sebekovsky@gmail.com>
2019-12-15 17:21:05 +0100

    Init CountsFromUser

---
+++ src/main/java/CountsFromUser.java
@@ -0,0 +1,61 @@
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.*;
+
+public class CountsFromUser {
+
+        String fileName = "Liczby.txt";
+        boolean moreCount = true;
+        Scanner scan = new Scanner(System.in);
+        Scanner scan1 = new Scanner(System.in);
+
+
+        public void doYouWantAddWelcome() {
+            System.out.println("Witaj, proszę podaj liczby, program wpisze liczby do pliku.");
+
+        }
+
+        public void doYouWantAdd() {
+
+            try {
+                FileOutputStream addToFileEveryLine = new FileOutputStream(fileName, true);
+                PrintWriter addCountToFile = new PrintWriter(addToFileEveryLine);
+
+                while (moreCount = true) {
+                    System.out.print("Podaj liczbę ");
+                    int count = scan.nextInt();
+                    System.out.println();
+                    addCountToFile.write(count + "\n");
+
+                    System.out.println("Czy chciałbyś dodać więcej liczb? (Wpisz >>Tak<< jeśli chcesz kontyunować)");
+                    String continueApp = scan1.nextLine().toUpperCase();
+
+                    if (continueApp.equals("TAK")) {
+                        moreCount = true;
+                    } else {
+                        moreCount = false;
+                        System.out.println("Zakończenie programu");
+                        break;
+                    }
+
+                }
+                addCountToFile.close();
+            }
+                 catch(IOException e){
+                        System.out.println("Error!");
+                    }
+
+            }
+        }
+
+
+
+
+
+
+
---
+++ src/main/java/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+
+        CountsFromUser countsFromUser = new CountsFromUser();
+        countsFromUser.doYouWantAddWelcome();
+        countsFromUser.doYouWantAdd();
+    }
+}
